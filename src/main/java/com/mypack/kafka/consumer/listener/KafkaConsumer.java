e9badca7d1aa14f6a2eab7d517bc440c23385980
Javed Alam <javedalam.in@gmail.com>
2022-03-26 20:02:21 +0530

    Updated Kafka Consumer to consume json message also messages from Kafka topic

--- src/main/java/com/mypack/kafka/consumer/listener/KafkaConsumer.java
+++ src/main/java/com/mypack/kafka/consumer/listener/KafkaConsumer.java
@@ -3,11 +3,18 @@ package com.mypack.kafka.consumer.listener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.mypack.kafka.consumer.model.User;
+
 @Service
 public class KafkaConsumer {
 
 	@KafkaListener(topics = "KafkaTopicExample", groupId = "group_id")
 	public void consume(String message) {
-		System.out.println("Consumed message: " + message);
+		System.out.println("Consumed Message: " + message);
 	}
+	
+	@KafkaListener(topics = "KafkaTopicExampleJson", groupId = "group_json", containerFactory = "userKafkaListenerContainerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
 }
---
+++ src/main/java/com/mypack/kafka/consumer/model/User.java
@@ -0,0 +1,18 @@
+package com.mypack.kafka.consumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+	private String name;
+	private String department;
+	private Long salary;
+}
